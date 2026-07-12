de63e714ec1d96c617a329507af5be0287a58ba3
Andrea Bergonzo <andybergon@gmail.com>
2026-07-12 06:01:43 +0100

    test: isolate Playwright web server

--- playwright.config.js
+++ playwright.config.js
@@ -9,12 +9,12 @@ export default defineConfig({
   fullyParallel: false,
   workers: 1,
   use: {
-    baseURL: "http://127.0.0.1:4173",
+    baseURL: "http://127.0.0.1:42873",
     screenshot: "only-on-failure",
   },
   webServer: {
-    command: "npm run dev -- --host 127.0.0.1 --port 4173",
-    url: "http://127.0.0.1:4173",
-    reuseExistingServer: true,
+    command: "npm run dev -- --host 127.0.0.1 --port 42873",
+    url: "http://127.0.0.1:42873",
+    reuseExistingServer: false,
   },
 });
